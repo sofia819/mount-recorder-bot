0ae3ce06463b1ad32feff20e6c3730fdf6d6dad6
Sofia Lee <sofia819@gmail.com>
2020-12-01 15:06:26 -0500

    sorting and mount name

--- index.js
+++ index.js
@@ -21,10 +21,19 @@ client.on('message', async (message) => {
         )
           .then((res) => res.json())
           .then((data) => {
-            const usersLackingMount = data
-              .filter((userMount) => !userMount.owned)
-              .map((userMount) => userMount.username);
-            message.channel.send(usersLackingMount.join('\n'));
+            const usersLackingMount = data.filter(
+              (userMount) => !userMount.owned
+            );
+            if (usersLackingMount.length > 0) {
+              const results =
+                usersLackingMount[0].mount_name +
+                '\n' +
+                usersLackingMount
+                  .map((userMount) => userMount.username)
+                  .sort((a, b) => a.localeCompare(b))
+                  .join('\n');
+              message.channel.send(results);
+            }
           })
       : message.channel.send('Format error: ' + '!mount mountId');
   }
